d8d3fdb939509771655eb3c54656289da6ec5ab7
jasarsyeds <134320016+jasarsyeds@users.noreply.github.com>
2023-05-23 00:23:01 +0530

    Add files via upload

---
+++ weddingscrpt.js
@@ -0,0 +1,12 @@
+// Get the button element by its id
+/* button = document.getElementById("redirectButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Perform the redirection
+  window.location.href = "https://www.example.com";
+});
+*/
+function myFunction() {
+    location.replace
+}
